d5f2a8b2c8b80cc388e721f6f7bc56b87c2c5434
SagarKumarDas407 <sagarkmar673@gmail.com>
2023-10-20 09:16:54 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", function(){
+
+    // const arrow =document.querySelector(".upper-arrow");
+    const slpoint=window.pageYOffset
+    console.log(slpoint)
+
+})
+
+let loader=document.querySelector(".loader");
+window.addEventListener("load", function(){
+    loader.style.display="none"
+})
+
+
+
